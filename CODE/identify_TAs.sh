5b3616b0dd785538f5e640036de11131e7d1827f
dgranadosv <83143839+dgranadosv@users.noreply.github.com>
2021-04-24 19:41:21 -0400

    Update identify_TAs.sh

--- CODE/identify_TAs.sh
+++ CODE/identify_TAs.sh
@@ -1,30 +1,30 @@
 # Iterate through all the text files and extract the sequence from the
 # uniprot files 
-#mkdir ../DATA/ids_first_filter
-#for folder in ../DATA/ids/*
-#do
-#awk '/SQ/,EOF' "$folder"/*.txt | head -n -1 | tail -n +1 | sed '0,/SQ/s//>SQ/' | tr -d ' ' > "$folder"/seq.fas
-#~/Desktop/tmhmm-2.0c/bin/tmhmm "$folder"/seq.fas > "$folder"/out.dat
-#mv TMH* "$folder"
+mkdir ../DATA/ids_first_filter
+for folder in ../DATA/ids/*
+do
+awk '/SQ/,EOF' "$folder"/*.txt | head -n -1 | tail -n +1 | sed '0,/SQ/s//>SQ/' | tr -d ' ' > "$folder"/seq.fas
+~/Desktop/tmhmm-2.0c/bin/tmhmm "$folder"/seq.fas > "$folder"/out.dat
+mv TMH* "$folder"
 # Filtering sequences that only have one TA
-#number_of_TAs=`grep "TMhelix" "$folder"/out.dat | wc -l`
-#if [ $number_of_TAs -eq 1 ]
-#then 
-#cp -r "$folder" '../DATA/ids_first_filter/'
-#fi
-#done
+number_of_TAs=`grep "TMhelix" "$folder"/out.dat | wc -l`
+if [ $number_of_TAs -eq 1 ]
+then 
+cp -r "$folder" '../DATA/ids_first_filter/'
+fi
+done
 # Filtering those proteins who have a far from the Cterminal 
-#mkdir ../DATA/ids_second_filter
-#for folder in ../DATA/ids_first_filter/*
-#do
-#len=`grep "Length" "$folder"/out.dat | awk '{print $4}'`
-#end=`grep "TMhelix" "$folder"/out.dat | awk '{print $5}'`
-#res=`expr $len - $end` 
-#if [ $res -le 30 ]
-#then
-#cp -r "$folder" ../DATA/ids_second_filter
-#fi
-#done
+mkdir ../DATA/ids_second_filter
+for folder in ../DATA/ids_first_filter/*
+do
+len=`grep "Length" "$folder"/out.dat | awk '{print $4}'`
+end=`grep "TMhelix" "$folder"/out.dat | awk '{print $5}'`
+res=`expr $len - $end` 
+if [ $res -le 30 ]
+then
+cp -r "$folder" ../DATA/ids_second_filter
+fi
+done
 # Run signalp to find signal sequences in the filtered proteins
 mkdir ../DATA/ids_third_filter
 for folder in ../DATA/ids_second_filter/*
